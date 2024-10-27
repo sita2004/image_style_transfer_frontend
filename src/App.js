0e6f87976979727a15193d37a441b5f3bcf19868
sita2004 <151060405+sita2004@users.noreply.github.com>
2024-10-27 20:09:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     setLoading(true); 
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/stylize', formData, {
+      const response = await axios.post('https://image-style-transfer-backend.onrender.com/stylize', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
